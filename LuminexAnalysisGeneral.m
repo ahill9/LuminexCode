f7abcd828197750d09a2e37c27b4cce7b2b866f5
Abby Hill <Abby Hill>
2016-08-05 18:06:31 -0400

    changed my mind, back to script

--- LuminexAnalysisGeneral.m
+++ LuminexAnalysisGeneral.m
@@ -1,7 +1,5 @@
-function LuminexAnalysisGeneral(analysisName, expDir, filename, standardLot, standardDilutions)
-
 %% Import Data
-cd(expDir) 
+cd(expDir)
 warning('off','MATLAB:xlswrite:AddSheet')
 beadCounts=readtable(filename,'Sheet','BeadCounts','ReadRowNames',false);
 beadCounts.Properties.VariableNames = regexprep(beadCounts.Properties.VariableNames,'[-_.]','');
@@ -44,7 +42,7 @@ dilutionFactor=table(standardMFIsorted{:,1},ones(length(standardMFIsorted{:,1}),
 for n = 1:length(standards)
     dilutionFactor{strcmp(dilutionFactor{:,1},standards(n)),2}=currDilution;
     currDilution = currDilution*standardDilutions;
-en
+end
 allStandardsConc = dilutionFactor{:,2}*standardConcSorted{1,2:end};
 allStandardsConc = [array2table(dilutionFactor{:,1}),array2table(allStandardsConc)];
 allStandardsConc.Properties.VariableNames = ['Standard',standardTable.Properties.VariableNames(2:end)];
@@ -93,4 +91,64 @@ meanSampleConcTab_1 = sampleConc_1(1:length(uniqueSampleNames),:);
 meanSampleConcTab_1{:,1} = uniqueSampleNames;
 meanSampleConcTab_1{:,2:end} = meanSampleConc_1;
 
+%% Method 2: Fit 5-parameter logistic curves to standards + blanks
+sampleMFIsorted=sampleMFIsortedSTABLE;
+standardMFIsorted=standardMFIsortedSTABLE;
+sampleConc_2 = sampleMFIsorted; sampleConc_2{1:end,2:end}=0;
+A=zeros(1,size(sampleConc_2,2)-1); B=A; C=A; D=A; E=A; %initialize
+flag=zeros(size(sampleConc_2,1),size(sampleConc_2,2)-1);
+sampleMFIadj=sampleMFIsorted;
+for c=1:size(sampleConc_2,2)-1
+    [cf{c},gf{c}] = L5P([allStandardsConc{1:end,c+1};blank0s(:,c+1)],[standardMFIsorted{1:end,c+1};blankMFIsorted{1:end,c+1}]); 
+    A(c)=cf{c}.A; B(c)=cf{c}.B; C(c)=cf{c}.C; D(c)=cf{c}.D; E(c)=cf{c}.E;
+    flag(sampleMFIsorted{:,c+1}<A(c),c)=-1; sampleMFIadj{sampleMFIsorted{:,c+1}<A(c),c+1}=A(c); 
+    flag(sampleMFIsorted{:,c+1}>D(c),c)=1; sampleMFIadj{sampleMFIsorted{:,c+1}>D(c),c+1}=0.99*D(c); 
+    sampleConc_2{:,c+1} = L5Pinv(cf{c},sampleMFIadj{:,c+1});
+    rsquares(c) = gf{c}.rsquare;
 end
+%Average replicates - assumes that sample replicates have identical names,
+%omit NaNs
+% **********************************************************************
+% To Do: check for bad wells (e.g. no sample added? all MFIs lower than
+% blank?)
+% **********************************************************************
+uniqueSampleNames = unique(sampleMFIsorted{:,1});
+meanSampleConc_2 = zeros(length(uniqueSampleNames),size(sampleConc_2,2)-1);
+for sampleIdx=1:length(uniqueSampleNames)
+    meanSampleConc_2(sampleIdx,:) = mean(sampleMFIsorted{strcmp(sampleMFIsorted{:,1},uniqueSampleNames(sampleIdx)),2:end},1,'omitnan');
+end
+meanSampleConcTab_2 = sampleConc_2(1:length(uniqueSampleNames),:);
+meanSampleConcTab_2{:,1} = uniqueSampleNames;
+meanSampleConcTab_2{:,2:end} = meanSampleConc_2;
+
+%% Method 3: Subtract blank MFI, then fit 5-parameter logistic curve (ignore negatives)
+sampleMFIsorted=sampleMFIsortedSTABLE;
+standardMFIsorted=standardMFIsortedSTABLE;
+sampleConc_3 = sampleMFIsorted; sampleConc_3{1:end,2:end}=0;
+A=zeros(1,size(sampleConc_3,2)-1); B=A; C=A; D=A; E=A; %initialize
+flag=zeros(size(sampleConc_3,1),size(sampleConc_3,2)-1);
+sampleMFIadj=sampleMFIsorted;
+sampleMFIsorted{:,2:end} = gsubtract(sampleMFIsorted{:,2:end},mean(blankMFIsorted{:,2:end}));
+standardMFIsorted{:,2:end}=gsubtract(standardMFIsorted{:,2:end},mean(blankMFIsorted{:,2:end}));
+for c=1:size(sampleConc_3,2)-1
+    [cf{c},gf{c}] = L5P([allStandardsConc{1:end,c+1};blank0s(:,c+1)],[standardMFIsorted{1:end,c+1};blankMFIsorted{1:end,c+1}]); 
+    A(c)=cf{c}.A; B(c)=cf{c}.B; C(c)=cf{c}.C; D(c)=cf{c}.D; E(c)=cf{c}.E;
+    flag(sampleMFIsorted{:,c+1}<A(c),c)=-1; sampleMFIadj{sampleMFIsorted{:,c+1}<A(c),c+1}=A(c); 
+    flag(sampleMFIsorted{:,c+1}>D(c),c)=1; sampleMFIadj{sampleMFIsorted{:,c+1}>D(c),c+1}=0.99*D(c); 
+    sampleConc_3{:,c+1} = L5Pinv(cf{c},sampleMFIadj{:,c+1});
+    rsquares(c) = gf{c}.rsquare;
+end
+%Average replicates - assumes that sample replicates have identical names,
+%omit NaNs
+% **********************************************************************
+% To Do: check for bad wells (e.g. no sample added? all MFIs lower than
+% blank?)
+% **********************************************************************
+uniqueSampleNames = unique(sampleMFIsorted{:,1});
+meanSampleConc_3 = zeros(length(uniqueSampleNames),size(sampleConc_3,2)-1);
+for sampleIdx=1:length(uniqueSampleNames)
+    meanSampleConc_3(sampleIdx,:) = mean(sampleMFIsorted{strcmp(sampleMFIsorted{:,1},uniqueSampleNames(sampleIdx)),2:end},1,'omitnan');
+end
+meanSampleConcTab_3 = sampleConc_3(1:length(uniqueSampleNames),:);
+meanSampleConcTab_3{:,1} = uniqueSampleNames;
+meanSampleConcTab_3{:,2:end} = meanSampleConc_3;
